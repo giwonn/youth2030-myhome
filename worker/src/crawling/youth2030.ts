ecdf9902da0eb36232c463301e7a71cf87bb7bb6
Allen / 임기원 <l4279625@gmail.com>
2022-02-27 14:05:07 +0900

    진행중

--- worker/src/crawling/youth2030.ts
+++ worker/src/crawling/youth2030.ts
@@ -1,10 +1,58 @@
 import { initBrowser, initPage } from './common';
 
+interface Location {
+  year: number;
+  num: number;
+  address: string;
+  nearBySubway: string;
+}
+
+interface SupplyCount {
+  total: number;
+  public: number;
+  private: number;
+}
+
+interface NoticeDate {
+  public: string;
+  private: string;
+}
+
+interface ConstructionPlan {
+  noticeDate: NoticeDate;
+  moveInDate: string;
+}
+
+interface HousingInfo {
+  location: Location;
+  supplyCount: SupplyCount;
+  constructionPlan: ConstructionPlan;
+  architect: string[];
+}
+
 export default async function crawling() {
-  const URL = 'http://youth2030.co.kr/user/board/mn010203.do';
+  const dbCount = 0;
+  const URL = 'http://youth2030.co.kr/user/page/mn010104.do';
 
   const browser = await initBrowser();
   const page = await initPage(browser, URL);
+
+  const table = [];
+  const tableCount = await page.locator('.status_apply_table > tbody > tr').count()
+
+  const tableList = await page.$$('.status_apply_table > tbody > tr')
+  for (let i = dbCount; i < tableCount; i++) {
+    table[i] = await tableList[i].$$eval('td', td => td.map((text) => text.textContent))
+  }
+
+  res.map(tr => {
+    tr.reduce((obj, td, i) => {
+
+    }, {})
+  })
+
+
+  console.log(table)
 
 }
 
