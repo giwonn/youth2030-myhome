1b0a9b61cd1505a2a9384e1ebabdb878bc93748a
Allen / 임기원 <l4279625@gmail.com>
2022-02-28 19:26:15 +0900

    [refactor] worker: supplyPlan.ts 리팩토링

--- worker/src/crawling/youth2030/supplyPlan.ts
+++ worker/src/crawling/youth2030/supplyPlan.ts
@@ -44,25 +44,26 @@ const getTableList = async (tableElement: ElementHandle[]) => {
   );
 }
 
-const getHousingInfo = (table: string[][]) => table.map(tr => {
-  const supplyPlan = new SupplyPlan();
-  tr.forEach((td, i) => {
-    const content = td?.trim();
-    if (i === 0) supplyPlan.location.year = Number(content);
-    else if (i === 1) supplyPlan.location.num = Number(content);
-    else if (i === 2) supplyPlan.location.address = content;
-    else if (i === 3 || i === 4) supplyPlan.location.address += ` ${ content }`;
-    else if (i === 5) supplyPlan.location.nearBySubway = content;
-    else if (i === 6) supplyPlan.supplyCount.total = Number(content);
-    else if (i === 7) supplyPlan.supplyCount.public = Number(content);
-    else if (i === 8) supplyPlan.supplyCount.private = Number(content);
-    else if (i === 9) supplyPlan.constructionPlan.noticeDate.public = content;
-    else if (i === 10) supplyPlan.constructionPlan.noticeDate.private = content;
-    else if (i === 11) supplyPlan.constructionPlan.moveInDate = content;
-    else if (i === 12) supplyPlan.architects = content;
-  });
-  return supplyPlan;
-});
+const getHousingInfo = (table: string[][]) => table.map((tr: string[]) =>
+  tr.reduce<SupplyPlan>((supplyPlan: SupplyPlan, td: string, idx: number) => {
+    (() => ({
+      0: () => supplyPlan.location.year = Number(td),
+      1: () => supplyPlan.location.num = Number(td),
+      2: () => supplyPlan.location.address = td,
+      3: () => supplyPlan.location.address += ` ${ td }`,
+      4: () => supplyPlan.location.address += ` ${ td }`,
+      5: () => supplyPlan.location.nearBySubway = td,
+      6: () => supplyPlan.supplyCount.total = Number(td),
+      7: () => supplyPlan.supplyCount.public = Number(td),
+      8: () => supplyPlan.supplyCount.private = Number(td),
+      9: () => supplyPlan.constructionPlan.noticeDate.public = td,
+      10: () => supplyPlan.constructionPlan.noticeDate.private = td,
+      11: () => supplyPlan.constructionPlan.moveInDate = td,
+      12: () => supplyPlan.architects = td,
+    }[idx]))()
+    return supplyPlan;
+  }, new SupplyPlan())
+);
 
 export default async function crawling() {
   const URL = 'http://youth2030.co.kr/user/page/mn010104.do';
